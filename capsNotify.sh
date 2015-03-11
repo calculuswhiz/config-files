c7ca7c8a4771ffde8786460cb10db7a8568d0774
calculuswhiz <calculuswhiz@users.noreply.github.com>
2015-03-11 16:44:15 -0500

    Update capsNotify.sh

--- capsNotify.sh
+++ capsNotify.sh
@@ -1,3 +1,7 @@
+# A caps-lock indicator for xfce4.
+# Shows the state of the caps-lock keyboard LED as a notification. I personally bound it to my caps-lock key.
+
+# Wait to read it accurately when caps-lock is pressed.
 sleep .2s
 
 if [[ $(xset q | grep -P LED.* -o | grep -P '(?<=[01]{7})[01]' -o) == 1 ]]; then
